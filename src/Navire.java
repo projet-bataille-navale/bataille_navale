d25b54ada487008fcdbadbfd4c1306eec1d05214
Saâd Talbi <talbisaad_007@hotmail.fr>
2014-12-13 15:19:22 +0100

    message

---
+++ src/Navire.java
@@ -0,0 +1,11 @@
+
+abstract class Navire {
+
+	int id , nbr_case;
+	String nom;
+
+	public Navire(int id) {
+		super();
+		this.id = id;
+	}
+}
---
+++ src/Zodiac.java
@@ -0,0 +1,13 @@
+
+public class Zodiac extends Navire {
+
+	public Zodiac(int id) {
+		super(id);
+		this.nbr_case=1;
+		this.nom = "Zodiac";
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
