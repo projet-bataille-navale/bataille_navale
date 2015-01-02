5f52bb1b44bec3d92ecf16f5080c114a8fb61a44
Saâd Talbi <talbisaad_007@hotmail.fr>
2015-01-02 12:16:01 +0100

    premiere_interface

---
+++ src/La_une.java
@@ -0,0 +1,101 @@
+import java.awt.EventQueue;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+
+
+//Class Test pour le premier interface
+public class La_une {
+
+	private JFrame frame;
+		PanelImage pnl_img;
+		private JButton btn_1j;
+		private JButton btn_2j;
+		private JButton btn_demo;
+		private JButton btn_q;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					La_une window = new La_une();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public La_une() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(300, 100,1300, 800);
+		pnl_img = new PanelImage();
+		pnl_img.setBounds(0, 0,1282, 755);
+		frame.getContentPane().add(pnl_img);
+		try {
+			pnl_img.setImage("bn.jpg");
+			pnl_img.setLayout(null);
+			
+			btn_1j = new JButton("Un Joueur");
+			btn_1j.setForeground(Color.ORANGE);
+			btn_1j.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 25));
+			btn_1j.setBounds(917, 430, 257, 45);
+			pnl_img.add(btn_1j);
+			
+			btn_1j.setContentAreaFilled(false);
+			
+			btn_2j = new JButton("Deux Joueur");
+			btn_2j.setForeground(Color.ORANGE);
+			btn_2j.setBounds(917, 517, 257, 45);
+			btn_2j.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 25));
+			btn_2j.setContentAreaFilled(false);
+			pnl_img.add(btn_2j);
+			
+			btn_demo = new JButton("Mode Démo");
+			btn_demo.setForeground(Color.ORANGE);
+			btn_demo.setBounds(917, 600, 257, 45);
+			btn_demo.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 25));
+			btn_demo.setContentAreaFilled(false);
+			pnl_img.add(btn_demo);
+			
+			btn_q = new JButton("Quitter");
+			 
+			btn_q.setForeground(Color.ORANGE);
+			btn_q.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 25));
+			btn_q.setContentAreaFilled(false);
+			btn_q.setBounds(917, 685, 257, 45);
+			pnl_img.add(btn_q);
+			
+			JLabel lblBatailleNavalle = new JLabel("Bataille Navalle");
+			lblBatailleNavalle.setFont(new Font("Algerian", Font.ITALIC, 38));
+			lblBatailleNavalle.setBounds(0, 0, 486, 84);
+			pnl_img.add(lblBatailleNavalle);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+}
---
+++ src/PanelImage.java
@@ -0,0 +1,66 @@
+
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+ 
+public class PanelImage extends JPanel {
+ 
+	private static final long serialVersionUID = -6350324456764768311L;
+
+	private Image image;
+
+	 
+	public PanelImage() {
+		super();
+	}
+	
+	 
+	public PanelImage(Image image) {
+		super();
+		setImage(image);
+	}
+
+	 
+	public PanelImage(String path) throws IOException {
+		super();
+		setImage(path);
+	}
+	
+	 
+	public void setImage(Image image) {
+		this.image = image;
+		repaint();
+	}
+	 
+	public void setImage(String path) throws IOException {
+		try {
+			this.image = ImageIO.read(new File(path));
+			repaint();
+		} 
+		catch (IOException e) {
+			throw new IOException(path+" introuvable", e);
+		}
+	}
+
+	 
+	public Image getImage() {
+		return image;
+	}
+	
+	
+	public void paintComponent(Graphics g){
+		if(image!=null){
+			Graphics2D g2d = (Graphics2D)g;
+			g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			g2d.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		}
+	}
+
+}
